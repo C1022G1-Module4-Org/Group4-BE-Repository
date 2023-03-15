866ca7f6125041ca1168c612204bb0c45e1137ad
Thanhtu2607 <117065047+C1022G-Thanh-Tu@users.noreply.github.com>
2023-03-15 14:37:53 +0700

    update project

--- cs_module/src/main/java/com/example/cs_module/service/coffee_shop/impl/CoffeeShopService.java
+++ cs_module/src/main/java/com/example/cs_module/service/coffee_shop/impl/CoffeeShopService.java
@@ -1,6 +1,6 @@
 package com.example.cs_module.service.coffee_shop.impl;
 
-import com.example.cs_be_module4.service.coffee_shop.ICoffeeShopService;
+import com.example.cs_module.service.coffee_shop.ICoffeeShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
